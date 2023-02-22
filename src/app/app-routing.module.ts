b7c7058ee81b6845295c6168d60dcded6cadf92b
Diogo Cerqueira <59118727+diogocerqueira-22002160@users.noreply.github.com>
2023-02-22 18:00:19 +0000

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home_tab',
+    redirectTo: 'splash-screen',
     pathMatch: 'full',
   },
   {
